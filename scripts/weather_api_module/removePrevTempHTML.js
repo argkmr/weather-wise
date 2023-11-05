2641186b3c7fa93cda119a6aff91f3c54a22d1bb
argkmr <125196042+argkmr@users.noreply.github.com>
2023-11-05 18:09:55 +0530

    once more

--- scripts/weather_api_module/removePrevTempHTML.js
+++ scripts/weather_api_module/removePrevTempHTML.js
@@ -1,12 +1,7 @@
 const removePrevTempHTML = () => {
-    const tempInfo = document.getElementById("tempInfo");
-    const subInfo = document.getElementById("sub-info");
 
     const container = document.getElementById("container");
-
-    if (container) {
-        container.remove();
-    }
+    if (container) container.remove();
 }
 
 export default removePrevTempHTML;
--- scripts/weather_api_module/weatherHandlerFuncs.js
+++ scripts/weather_api_module/weatherHandlerFuncs.js
@@ -4,13 +4,7 @@ import weatherDataHandler from "./weatherDataHandler.js";
 const weatherHandlerFuncs = () => {
     const welcome = document.getElementById("welcome");
     if (welcome) welcome.remove();
-
-    // const tempInfo = document.getElementById("tempInfo");
-    // const subInfo = document.getElementById("sub-info");
-
-    // const container = document.getElementById("container");
-
-    // if (container) 
+    
     removePrevTempHTML();
     weatherDataHandler();
 }
